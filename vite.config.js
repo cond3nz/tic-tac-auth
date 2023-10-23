c03407a39076ccda659433dce13dbd7a4742e088
cond3nz <tay93376@yandex.ru>
2023-10-23 11:24:17 +0300

    add manifest to vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,26 @@ export default defineConfig({
     react(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      injectRegister: 'auto'
+      injectRegister: 'auto',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'cond3nz tic-tac-auth',
+        short_name: 'TTA',
+        description: 'Zaebalsya',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
      })
   ],
   
