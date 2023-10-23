fa83ff8a9f55d2dc32ae00221ae14cd54f48dfc4
cond3nz <tay93376@yandex.ru>
2023-10-23 17:39:47 +0300

    css modificate

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
         icons: [
           {
             src: 'src/assets/react.svg',
-            sizes: '48x48 72x72 96x96 128x128 256x256',
+            sizes: "any",
             type: 'image/svg+xml',
             purpose: "any"
           }
